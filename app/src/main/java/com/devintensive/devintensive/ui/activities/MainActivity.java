d97e3384ede6435c7318e00229ff5b669e4956b6
GrekNMD <greknmd@gmail.com>
2016-06-20 21:04:59 +0300

    DevIntensive
    Dev intensive

---
+++ app/src/main/java/com/devintensive/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.devintensive.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.devintensive.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
