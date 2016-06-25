c50fb0e3bca1b81a728dac1c7133e5ef5fa87363
GrekNMD <greknmd@gmail.com>
2016-06-25 03:06:59 +0300

    added icon,style,chenged mainActivity

--- app/src/main/java/com/devintensive/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/devintensive/devintensive/ui/activities/MainActivity.java
@@ -2,14 +2,75 @@ package com.devintensive.devintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.devintensive.devintensive.R;
+import com.devintensive.devintensive.utils.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG= ConstantManager.TAG_PREFIX+"Main Activity";
+
+    //UI interface
+    //initializing static parameters
+    //@param savedInstanceState - object with parameters saved in Bundle - state UI
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        if (savedInstanceState == null){
+            //1st time activity start
+        }
+        else {
+            //activity was created
+        }
+    }
+
+
+
+    //metod starts on activity start be for UI is visible
+    //
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+
+    //metod starts after UI visible
+    //here using audio/video/broadcast receiver
+    //metod have to be as light as posible
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 }
---
+++ app/src/main/java/com/devintensive/devintensive/utils/ConstantManager.java
@@ -0,0 +1,9 @@
+package com.devintensive.devintensive.utils;
+
+/**
+ * Created by grekn_000 on 6/24/2016.
+ */
+public interface ConstantManager {
+
+    String TAG_PREFIX="DEV ";
+}
