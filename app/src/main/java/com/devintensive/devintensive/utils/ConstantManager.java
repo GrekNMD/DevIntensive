fecafc4f1f63379976b1fbfc1f5b9db62ecfe3a6
GrekNMD <greknmd@gmail.com>
2016-06-29 07:46:30 +0300

    Added Navigation Drawer,Collapsing toolbar,load and save user info...Also have some problem in setupDrawer(),can't see the problem...May be you can help me??

--- app/src/main/java/com/devintensive/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/devintensive/devintensive/utils/ConstantManager.java
@@ -1,9 +1,16 @@
 package com.devintensive.devintensive.utils;
 
-/**
- * Created by grekn_000 on 6/24/2016.
- */
+
 public interface ConstantManager {
 
     String TAG_PREFIX="DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+
+    String EDIT_MOD_KEY="EDIT_MOD_KEY";
+
+    String USER_PHONE_KEY = "USER_PHONE_KEY";
+    String USER_EMAIL_KEY = "USER_EMAIL_KEY";
+    String USER_VK_KEY = "USER_VK_KEY";
+    String USER_GIT_KEY = "USER_GIT_KEY";
+    String USER_ABOUT_KEY = "USER_ABOUT_KEY";
 }
